5fe910ec11f797d4eb6a670867bbc6f5f3f30e76
RaghuwanshiAditya <iam.libfront@gmail.com>
2025-11-29 18:27:50 +0530

    Nov, 29, 2025
    Nov, 29, 2025

---
+++ tools/ssl_checker/dist/renderer/src/foo.d.ts
@@ -0,0 +1,5 @@
+import { _p_TYP, _$cb_TYP, _$p_TYP } from '../../shared/types';
+declare const index: (_p: _p_TYP) => {
+    set: (_$p: _$p_TYP, _$cb?: _$cb_TYP) => string;
+};
+export { index as foo };
